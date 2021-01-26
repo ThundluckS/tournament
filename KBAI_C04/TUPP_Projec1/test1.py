38571a7d82f4ded17cb9a8ca467fca3040ef00cd
Thundluck <thundluck.s@gmail.com>
2021-01-26 19:20:53 +0700

    add KBAI_C04 projects

---
+++ KBAI_C04/TUPP_Projec1/test1.py
@@ -0,0 +1,26 @@
+word = None
+
+
+from geometry_msgs.msg import Twist
+import rospy
+import time
+rospy.init_node('get_center', anonymous=True)
+velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+vel_msg = Twist()
+import roslib
+import rospy
+from kidbright_tpu.msg import tpu_object
+from kidbright_tpu.msg import tpu_objects
+from std_msgs.msg import String
+import rosnode
+import subprocess
+import time
+import os
+ros_nodes = rosnode.get_node_names()
+if not '/wake_class_wait' in ros_nodes:
+	command='rosrun kidbright_tpu wakeword_classify.py _terminate:=False _model:=/home/pi/kbclientNew/nectec-client/dist/TUPP_Projec1/audios/model.h5 label_file:=/home/pi/kbclientNew/nectec-client/dist/TUPP_Projec1/audios/label_map.pkl _nframe:=12'
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	time.sleep(10)
+while not rospy.is_shutdown():
+  word = rospy.wait_for_message('/inference', String, timeout=4).data
+  print(word)
